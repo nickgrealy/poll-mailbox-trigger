ddba4e5955f758489eb934eced63ba12c126e962
Nick Grealy <nickgrealy@gmail.com>
2016-08-06 10:24:31 +1000

    Modified EnvVarsAction to use QueueAction instead of ParametersAction.

--- src/main/groovy/org/jenkinsci/plugins/pollmailboxtrigger/environment/SetEnvironmentVariablesAction.java
+++ src/main/groovy/org/jenkinsci/plugins/pollmailboxtrigger/environment/SetEnvironmentVariablesAction.java
@@ -1,43 +1,53 @@
 package org.jenkinsci.plugins.pollmailboxtrigger.environment;
 
 import hudson.model.Action;
-import hudson.model.ParameterValue;
-import hudson.model.ParametersAction;
-import hudson.model.StringParameterValue;
+import hudson.model.Queue;
+import hudson.model.Queue.QueueAction;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 /**
  * This wrapper is needed since SECURITY-170 is blocking undefined build parameters in the job.
  */
-public class SetEnvironmentVariablesAction extends ParametersAction implements Action {
+public class SetEnvironmentVariablesAction implements QueueAction {
 
     private final Map<String, String> parameters;
-    private final Map<String, ParameterValue> values;
 
     public SetEnvironmentVariablesAction(final Map<String, String> parameters) {
         this.parameters = parameters;
-        this.values = new HashMap<>();
-        for (Map.Entry<String, String> entry : parameters.entrySet()) {
-            this.values.put(entry.getKey(), new StringParameterValue(entry.getKey(), entry.getValue()));
-        }
-    }
-
-    @Override
-    public List<ParameterValue> getParameters() {
-        return new ArrayList<>(values.values());
-    }
-
-    @Override
-    public ParameterValue getParameter(final String name) {
-        return values.get(name);
     }
 
     public Map<String, String> getParametersAsMap() {
-        return this.parameters;
+        return parameters;
+    }
+
+    // interface -> hudson.model.Action
+
+    @Override
+    public String getIconFileName() {
+        // don't create a link to content...
+        return null;
+    }
+
+    @Override
+    public String getDisplayName() {
+        // don't create a link to content...
+        return null;
+    }
+
+    @Override
+    public String getUrlName() {
+        // don't create a link to content...
+        return null;
+    }
+
+    // interface -> hudson.model.Queue.QueueAction
+
+    @Override
+    public boolean shouldSchedule(List<Action> actions) {
+        // always execute task...
+        return true;
     }
 
 }
